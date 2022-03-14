933f20a63a0cdb32743f69f4ad9a98448ea301ea
santiasecas <santiasecas@gmail.com>
2022-03-14 11:50:52 +0100

    Modifico tabs por espacios

--- main.py
+++ main.py
@@ -30,63 +30,63 @@ def limpiar_texto(texto):
     return ' '.join(texto.strip().replace('\n','').replace('\t','').split())
 
 def recupera_info_euromillon():
-	global solicitud
-	res = ''
-	try:
-		br = mechanize.Browser()
-		br.open(URL)
-		br.select_form(nr=0)
-		br.form['username'] = USUARIO
-		br.form['password'] = PASSWORD
-		br.submit()
+    global solicitud
+    res = ''
+    try:
+        br = mechanize.Browser()
+        br.open(URL)
+        br.select_form(nr=0)
+        br.form['username'] = USUARIO
+        br.form['password'] = PASSWORD
+        br.submit()
 
-		pagina = br.open(URL_2).read()
-		sopa = bs(pagina, features="html5lib")
+        pagina = br.open(URL_2).read()
+        sopa = bs(pagina, features="html5lib")
 
-		res  = 'Saldo actual: '
-		   
-		cantidad = sopa.find("span",{"titulo-valor"})
-		res += limpiar_texto(cantidad.text) + '\n\n'
+        res  = 'Saldo actual: '
+           
+        cantidad = sopa.find("span",{"titulo-valor"})
+        res += limpiar_texto(cantidad.text) + '\n\n'
 
-		res += 'Últimas 5 apuestas:\n'
+        res += 'Últimas 5 apuestas:\n'
 
-		apuestas = sopa.findAll("li",{"decripcion oculta_mobile last"})
-		cont = 1
+        apuestas = sopa.findAll("li",{"decripcion oculta_mobile last"})
+        cont = 1
 
-		for apuesta in apuestas:
-			res += f'Apuesta {str(cont)}:\n'
-			res += limpiar_texto(apuesta.text) + '\n'
-			cont += 1
-	except:
-		res = 'Ha fallado la recuperación de información'
-	finally:
-		solicitud = False
-	return res
+        for apuesta in apuestas:
+            res += f'Apuesta {str(cont)}:\n'
+            res += limpiar_texto(apuesta.text) + '\n'
+            cont += 1
+    except:
+        res = 'Ha fallado la recuperación de información'
+    finally:
+        solicitud = False
+    return res
 
 def obtener_informacion_euromillon(update, context):
-	global solicitud
-	if solicitud == False:
-		update.message.reply_text('Se está consultando la información... Espera un momento.')
-		res = recupera_info_euromillon()
-		update.message.reply_text(res)
-	elif solicitud == True:
-		update.message.reply_text('Ya has hecho una consulta. Espera la respuesta')
+    global solicitud
+    if solicitud == False:
+        update.message.reply_text('Se está consultando la información... Espera un momento.')
+        res = recupera_info_euromillon()
+        update.message.reply_text(res)
+    elif solicitud == True:
+        update.message.reply_text('Ya has hecho una consulta. Espera la respuesta')
 
 def main():
     TOKEN = os.environ['BOTTOKEN']
     NAME  = os.environ['NAME']
     PORT  = os.environ.get('PORT')
-	PORT = int(os.environ.get('PORT', '8443'))
-	
+    PORT = int(os.environ.get('PORT', '8443'))
+    
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('saldo', obtener_informacion_euromillon))
-
+    
     updater.start_webhook(listen="0.0.0.0",port=PORT,url_path=TOKEN)
     updater.bot.webhook_url(f"https://{NAME}.herokuapp.com/{TOKEN}")
-	
+    
     updater.idle()
 
 if __name__ == '__main__':
