43aab336f873768231e1e585856caae1e681fabf
santiasecas <santiasecas@gmail.com>
2022-03-14 10:24:45 +0100

    Primera subida

---
+++ README.md
@@ -0,0 +1,3 @@
+# Info Euromillón
+Bot para Telegram que consulta el saldo y últimas apuestas en la web de Loterías y Apuestas del estado.
+Hay que añadir en variables del entorno el/los usuario/s de Telegram que tiene permiso de consulta.
---
+++ main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+import mechanize
+
+from bs4 import BeautifulSoup as bs
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+ADMIN    = os.environ['ADMIN']
+USUARIO  = os.environ['USUARIO']
+PASSWORD = os.environ['PASSWORD']
+
+URL   = "https://juegos.loteriasyapuestas.es/acceder/login"
+URL_2 = 'https://juegos.loteriasyapuestas.es:443/jugar/cas/apuestas/historicos'
+
+solicitud = False
+
+def start(update, context):
+    nuevoUsuario = str(update.message.from_user.username)
+    context.bot.send_message(ADMIN,'Nuevo usuario: @' + nuevoUsuario)
+    #update.message.reply_text('Welcome ' + nuevoUsuario + '\nYou can contact me at @santiasecas')
+
+def limpiar_texto(texto):
+    return ' '.join(texto.strip().replace('\n','').replace('\t','').split())
+
+def recupera_info_euromillon():
+	global solicitud
+	res = ''
+	try:
+		br = mechanize.Browser()
+		br.open(URL)
+		br.select_form(nr=0)
+		br.form['username'] = USUARIO
+		br.form['password'] = PASSWORD
+		br.submit()
+
+		pagina = br.open(URL_2).read()
+		sopa = bs(pagina, features="html5lib")
+
+		res  = 'Saldo actual: '
+		   
+		cantidad = sopa.find("span",{"titulo-valor"})
+		res += limpiar_texto(cantidad.text) + '\n\n'
+
+		res += 'Últimas 5 apuestas:\n'
+
+		apuestas = sopa.findAll("li",{"decripcion oculta_mobile last"})
+		cont = 1
+
+		for apuesta in apuestas:
+			res += f'Apuesta {str(cont)}:\n'
+			res += limpiar_texto(apuesta.text) + '\n'
+			cont += 1
+	except:
+		res = 'Ha fallado la recuperación de información
+	finally:
+		solicitud = False
+	return res
+
+def obtener_informacion_euromillon(update, context):
+	global solicitud
+	if solicitud == False:
+		update.message.reply_text('Se está consultando la información... Espera un momento.')
+		res = recupera_info_euromillon()
+		update.message.reply_text(res)
+	elif solicitud == True:
+		update.message.reply_text('Ya has hecho una consulta. Espera la respuesta')
+
+def main():
+    TOKEN = os.environ["BOTTOKEN"]
+    NAME = os.environ["NAME"]
+    PORT = os.environ.get('PORT')
+    
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler('saldo', obtener_informacion_euromillon))
+
+    updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    updater.idle()
+    
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
